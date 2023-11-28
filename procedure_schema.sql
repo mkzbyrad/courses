4f15e2c7dc11faf3c25660d3e60c12d7cb472627
Winterz349 <31893271+Winterz349@users.noreply.github.com>
2023-11-28 10:34:10 +0100

    Update procedure_schema.sql

--- procedure_schema.sql
+++ procedure_schema.sql
@@ -1,24 +1,28 @@
-CREATE OR REPLACE _________ `<<PROYECT_ID>>.<<DATASET_ID>>.count_procedure`(
- IN _____ STRING,
- IN _____ INT64,
- IN _____ INT64
+CREATE OR REPLACE PROCEDURE `bigquery-learning-405922.airbnb.count_procedure`(
+  IN table_name_param STRING,
+  IN year_param INT64,
+  IN month_param INT64
 )
 BEGIN
- _______ dynamic_sql STRING;
- _______ review_count INT64;
- SET __________ = _____________(
-  '''
-  SELECT COUNT(1) AS review_count
-  FROM `__`
-  WHERE
-   EXTRACT(YEAR FROM DATE_TRUNC(date, MONTH)) = __
-   AND EXTRACT(MONTH FROM DATE_TRUNC(date, MONTH)) = __
-  ''',
-  ______, year_param, month_param
- );
- EXECUTE IMMEDIATE ___________ INTO ___________;
- SELECT _________;
+  DECLARE dynamic_sql STRING;
+  DECLARE review_count INT64;
+
+  SET dynamic_sql = FORMAT(
+    '''
+    SELECT COUNT(1) AS review_count
+    FROM `%s`
+    WHERE
+      EXTRACT(YEAR FROM DATE_TRUNC(date, MONTH)) = %d
+      AND EXTRACT(MONTH FROM DATE_TRUNC(date, MONTH)) = %d
+    ''',
+    table_name_param, year_param, month_param
+  );
+
+  EXECUTE IMMEDIATE dynamic_sql INTO review_count;
+
+  SELECT review_count;
 END;
--- Llamada
-____ `<<PROYECT_ID>>.<<DATASET_ID>>.count_procedure`('<<PROYECT_ID>>.<<DATASET_ID>>.reviews_p', 2023, 1);
+
+-- Ejemplo de uso
+CALL `bigquery-learning-405922.airbnb.count_procedure`('bigquery-learning-405922.airbnb.reviews_p', 2018, 1);
 
